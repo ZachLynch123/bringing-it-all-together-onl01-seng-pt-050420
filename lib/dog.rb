e090ad11b1c33383f0753196d6b950b6c6812be8
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-14 21:15:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,8 +60,8 @@ class Dog
       VALUES (?,?)
       SQL
       
-      DB[:conn].execute(sql, self.id, self.name, self.breed)
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 
    end
       
