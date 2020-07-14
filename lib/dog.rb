e23951ba843343a15aa05e34de4e46b238d2bc49
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-14 21:06:01 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,56 @@
+require 'pry'
+class Dog 
+  
+  attr_accessor :name, :breed
+  attr_reader :id 
+  
+  def initialize(id: nil, name:, breed:)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.new_from_db(row)
+    binding.pry
+    new_dog = Dog.new(row[0], row[1], row[2])
+    new_dog
+  end
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
